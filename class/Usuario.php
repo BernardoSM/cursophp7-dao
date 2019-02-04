22fa05b0b503b8e46bc78889219302e64d031133
Bernardo Simonassi <bernardosimonassi@hotmail.com>
2019-02-04 09:58:15 -0200

    metodos de lista

--- class/Usuario.php
+++ class/Usuario.php
@@ -43,7 +43,7 @@ class Usuario {
 
 		$results = $sql->select(" SELECT * FROM tb_usuarios WHERE idusuario = :ID", array(
 			":ID"=>$id
-		)); //função do banco para puxar do banco o idusuario da tabela tb_usuario
+		)); //função do banco para puxar do banco o idusuario da tabela tb_usuario (apenas um usuário)
 
 		if (count($results) > 0){ //tratamento de erro para selecionar somente os tb_usuarios existentes
 			$row = $results[0];
@@ -52,6 +52,43 @@ class Usuario {
 			$this->setDesLogin($row["deslogin"]);
 			$this->setDesSenha($row["dessenha"]);
 			$this->setDtCadastro(new DateTime($row["dtcadastro"]));
+		}
+	}
+
+	//FUNÇÃO QUE RETORNA A LISTA DE TODOS OS USUÁRIOS DO BANCO
+	public static function getList(){
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin;"); //essa função chama todos os usuários da tabela
+	}
+
+	//FUNÇÃO QUE BUSCA OS USUÁRIOS NO BANCO
+	public static function search($login){
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(
+			':SEARCH'=>"%".$login."%"
+		));
+	}
+
+	//FUNÇÃO QUE PEDE PRO USUÁRIO COLOCAR LOGIN E SENHA
+	public function login($login, $password){
+		$sql = new Sql(); //chamar o sql para fazer a função do banco
+
+		$results = $sql->select(" SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD", array(
+			":LOGIN"=>$login,
+			":PASSWORD"=>$password
+		)); //função do banco para puxar do banco o idusuario da tabela tb_usuario (apenas um usuário)
+
+		if (count($results) > 0){ //tratamento de erro para aceitar o login e senha existentes na tabela tb_usuarios
+			$row = $results[0];
+
+			$this->setIdUsuario($row["idusuario"]);
+			$this->setDesLogin($row["deslogin"]);
+			$this->setDesSenha($row["dessenha"]);
+			$this->setDtCadastro(new DateTime($row["dtcadastro"]));
+		} else { // caso o login e senha estejam incorretos aparecerá a seguinte mensagem
+			throw new Exception("Login e/ou senha inválidos.");
 		}
 	}
 
--- index.php
+++ index.php
@@ -2,10 +2,24 @@
 
 require_once("config.php"); //o arquivo config chama todas as classes (em arquivos diferentes)
 
-$root = new Usuario(); //a variavel root receberá o tratamento da classe Usuario, que no caso foi criada para chamar todos os valores da tabela e retorná-los para serem exibidos
 
-$root->loadbyID(3); //o loadbyId é a classe que seleciona em qual índice de usuário eu quero as informações
+// CARREGA UM USUÁRIO
+//$root = new Usuario(); //a variavel root receberá o tratamento da classe Usuario, que no caso foi criada para chamar todos os valores da tabela e retorná-los para serem exibidos
+//$root->loadbyID(3); //o loadbyId é a classe que seleciona em qual índice de usuário eu quero as informações (apenas um usuário)
+//echo $root;
 
-echo $root;
+//CARREGA UMA LISTA DE USUÁRIOS
+//$lista = Usuario::getList();
+//echo json_encode($search);
+
+//CARREGA UMA LISTA DE USUÁRIOS BUSCANDO PELO LOGIN
+//$search = Usuario::search("jo"); //retorna os usuários que começam com "jo"
+//echo json_encode($search);
+
+//CARREGA UM USUÁRIO USANDO O LOGIN E A SENHA
+$usuario = new Usuario();
+$usuario->login("jose", "1234567");
+
+echo $usuario;
 
  ?>
